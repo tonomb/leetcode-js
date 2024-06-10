1f3e6f6f6d3b0e421ce6da01f9c0493cb0fad3ee
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-10 14:16:44 -0600

    implemented bfs

--- src/dataStructures/graph/breathFirstSeach.js
+++ src/dataStructures/graph/breathFirstSeach.js
@@ -1,1 +1,42 @@
 // Given a directed graph represented as an adjacency list, graph, and an integer, source, which is the starting vertex number, return an array of integers, result, that contains the order of the graph’s breadth-first traversal starting from the source vertex.
+
+import { Queue } from "./utils/index.js";
+import { Graph } from "./graph.js";
+
+export function bfsTraversal(graph, source) {
+  let result = [];
+  let graphQueue = new Queue(graph.vertices);
+
+  // Keep track of visited vertex
+  let visited = new Array(graph.vertices).fill(false);
+
+  // Enqueue the source vertex and mark it as visited
+  graphQueue.enqueue(source);
+  visited[source] = true;
+
+  // while queue is not empty
+  while (graphQueue.isEmpty() == false) {
+    // get element from queue
+    let currentVertex = graphQueue.dequeue();
+    // add data to reulst arrau
+    result.push(currentVertex);
+
+    // push all neighbors into queue
+    let currentNode = graph.list[currentVertex].getHead();
+
+    while (currentNode != null) {
+      if (visited[currentNode.data] == false) {
+        graphQueue.enqueue(currentNode.data);
+
+        // Mark vertex as visited.
+        visited[currentNode.data] = true;
+      }
+
+      currentNode = currentNode.next;
+    }
+  }
+
+  // once queue is empty retun results array
+
+  return result;
+}
--- src/dataStructures/graph/graph.js
+++ src/dataStructures/graph/graph.js
@@ -65,11 +65,3 @@ export class Graph {
     return str;
   }
 }
-
-let g = new Graph(4);
-
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 3);
-g.addEdge(2, 3);
-g.printGraph();
