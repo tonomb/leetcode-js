423dc325c0a152dea6f2825566d058f30ea30cfa
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-11 11:17:51 -0600

    added stack to Graph utils

---
+++ src/dataStructures/graph/depthFirstSearch.js
@@ -0,0 +1,4 @@
+// Given a directed graph represented by an adjacency array graph and an integer source, representing the start vertex of the graph, return an array of integers, result that shows the order of depth-first traversal starting from the specified source vertex.
+
+import { Graph } from "./graph";
+import { Queue } from "./utils/index.js";
