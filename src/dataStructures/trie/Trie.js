9e1622d78ebeb34e11cf12bf33c49f08f3f3c2d0
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-12 18:31:09 -0600

    implement delete in trie

--- src/dataStructures/trie/Trie.js
+++ src/dataStructures/trie/Trie.js
@@ -98,7 +98,7 @@ export class Trie {
       currentNode = currentNode.children[index];
     }
 
-    if (currentNode != null && currentNode.isEndWord()) {
+    if (currentNode != null && currentNode.isEndWord) {
       return true;
     }
 
@@ -106,7 +106,74 @@ export class Trie {
   }
 
   delete(key) {
-    return null;
+    if (this.root == null || key == null) {
+      console.log("Nonde key or empty trie error");
+      return;
+    }
+
+    this.deleteHelper(key, this.root, key.length, 0);
+  }
+
+  //Recursive function to delete given key
+  deleteHelper(key, currentNode, length, level) {
+    let deletedSelf = false;
+
+    if (currentNode == null) {
+      console.log("Key does not exist");
+      return deletedSelf;
+    }
+
+    //Base Case: If we have reached at the node which points to the alphabet at the end of the key.
+    if (level == length) {
+      //If there are no nodes ahead of this node in this path
+      //Then we can delete this node
+      if (this.hasNoChildren(currentNode)) {
+        currentNode = null;
+        deletedSelf = true;
+      }
+
+      //If there are nodes ahead of currentNode in this path
+      //Then we cannot delete currentNode. We simply unmark this as leaf
+      else {
+        currentNode.unMarkAsLeaf();
+        deletedSelf = false;
+      }
+    } else {
+      let childNode = currentNode.children[this.getIndex(key[level])];
+      let childDeleted = this.deleteHelper(key, childNode, length, level + 1);
+      if (childDeleted) {
+        //Making children pointer also None: since child is deleted
+        currentNode.children[this.getIndex(key[level])] = null;
+        //If currentNode is leaf node that means currentNode is part of another key
+        //and hence we can not delete this node and it's parent path nodes
+        if (currentNode.isEndWord) {
+          deletedSelf = false;
+        }
+
+        //If childNode is deleted but if currentNode has more children then currentNode must be part of another key
+        //So, we cannot delete currenNode
+        else if (this.hasNoChildren(currentNode) == false) {
+          deletedSelf = false;
+        }
+        //Else we can delete currentNode
+        else {
+          currentNode = null;
+          deletedSelf = true;
+        }
+      } else {
+        deletedSelf = false;
+      }
+    }
+    return deletedSelf;
+  }
+
+  hasNoChildren(currentNode) {
+    for (let i = 0; i < currentNode.children.length; i++) {
+      if (currentNod.children[i] != null) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
