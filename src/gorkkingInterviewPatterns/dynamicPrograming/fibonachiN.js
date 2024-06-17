74989fa6d8f6729289cc56a025bdbaffae60a67a
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-16 19:02:58 -0600

    refactored fib to include memoization

--- src/gorkkingInterviewPatterns/dynamicPrograming/fibonachiN.js
+++ src/gorkkingInterviewPatterns/dynamicPrograming/fibonachiN.js
@@ -1,10 +1,32 @@
-function fib(n) {
+// function fib(n) {
+//   if (n <= 2) {
+//     return 1;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// // O(2^n) too slow
+
+// Memoization
+
+// Use a JS Obect to retreive seen numbers
+//  Keys will be arg to finally, value will be the return value
+
+const fib = (n, memo = {}) => {
+  if (n in memo) {
+    return memo[n];
+  }
+
   if (n <= 2) {
     return 1;
   }
-  return fib(n - 1) + fib(n - 2);
-}
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
+// O(n)
 
 console.log(fib(6));
 console.log(fib(7));
 console.log(fib(8));
+console.log(fib(50));
