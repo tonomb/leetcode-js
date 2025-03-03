a6dab30561b0531e4c639307c49a9848ef602cb5
Antonio MB <tono.mtzb@gmail.com>
2025-03-03 14:11:49 -0600

    feat: complets problem 1

---
+++ src/crackingCodingInterview/arraysAndStrings/1.1-is-unique.js
@@ -0,0 +1,20 @@
+// Implement an algorithm to determine if a string has all unique characters. What if you cannot use aditional structures?
+
+function isUniqueChars(str) {
+  if (str.length > 128) return false;
+
+  const char_set = Array(128).fill(false);
+  for (let i = 0; i < str.length; i++) {
+    const val = str.charAt(i);
+    if (char_set[val]) {
+      return false;
+    }
+    char_set[val] = true;
+  }
+  return true;
+}
+
+console.log(isUniqueChars("name"));
+
+// Time Complecity O(n) where n is the length of the string.
+// Space Complecity O(1)
